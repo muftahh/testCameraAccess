38c42b1eb0912d09b36c953df41bbffe5d6288d9
muftahh <muftahnaser54321@gmail.com>
2024-02-22 10:42:48 +0700

    Initial commit

---
+++ app/src/main/java/com/hacktivate8/testcameraaccess/MainActivity.java
@@ -0,0 +1,96 @@
+package com.hacktivate8.testcameraaccess;
+
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.FileProvider;
+
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.hacktivate8.testcameraaccess.databinding.ActivityMainBinding;
+
+import java.io.File;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final int CAMERA_PERMISSION_CODE = 1;
+    ActivityMainBinding mainBinding;
+    ActivityResultLauncher<Uri> takePictureLauncher;
+    Uri imageUri;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mainBinding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(mainBinding.getRoot());
+
+        imageUri = createUri();
+        registerPicturesLauncher();
+
+        mainBinding.btnCamera.setOnClickListener( view -> {
+            checkPermission();
+        });
+    }
+
+    private Uri createUri(){
+        File imageFile = new File(getApplicationContext().getFilesDir(), "camera_photo.jpg");
+        return FileProvider.getUriForFile(
+                getApplicationContext(),
+                "com.hacktivate8.testcameraaccess.fileProvider",
+                imageFile
+        );
+    }
+
+    private void registerPicturesLauncher(){
+        takePictureLauncher = registerForActivityResult(
+                new ActivityResultContracts.TakePicture(),
+                new ActivityResultCallback<Boolean>() {
+                    @Override
+                    public void onActivityResult(Boolean result) {
+                        try {
+                            if (result) {
+                                mainBinding.ivUser.setImageURI(null);
+                                mainBinding.ivUser.setImageURI(imageUri);
+                            }
+                        } catch (Exception exception) {
+                            exception.getStackTrace();
+                        }
+                    }
+                }
+        );
+    }
+
+    private void checkPermission() {
+        if (ActivityCompat.checkSelfPermission(MainActivity.this,
+                Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(MainActivity.this,
+                    new String[]{Manifest.permission.CAMERA}, CAMERA_PERMISSION_CODE);
+        }
+        else {
+            takePictureLauncher.launch(imageUri);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (requestCode == CAMERA_PERMISSION_CODE) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                takePictureLauncher.launch(imageUri);
+            }
+            else {
+                Toast.makeText(this, "Acess DENIED", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+}
